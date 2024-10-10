76a392b915804b343c846f4c5c87392bbf667689
Roxy <1012436741@qq.com>
2024-10-10 10:19:30 +0800

    first commit

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,34 @@
+Component({
+  data: {
+    chans:false,
+    selected:0,
+    color: "#dbdbdb",
+    selectedColor: "#2c2c2c",
+    list: [{
+      "pagePath": "/pages/index/index",
+      "iconPath": "/img/1.png",
+      "selectedIconPath": "/img/11.png",
+      "text": "首页"
+    }
+    , {
+      "pagePath": "/pages/logs/logs",
+      "iconPath": "/img/2.png",
+      "selectedIconPath": "/img/22.png",
+      "text": "个人"
+    }]
+  },
+  attached() {
+  },
+  methods: {
+    
+    switchTab(e) {
+      //console.log(e)
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  },
+})
